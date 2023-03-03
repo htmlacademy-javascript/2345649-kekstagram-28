0a29f78cd55981efebfa6e4ff48cecdfdcf16e9e
EYE <EYE@EYEz>
2023-03-03 18:02:40 +0300

    Исправит ошибки линтера о неиспользуемых функциях.

--- js/functions.js
+++ js/functions.js
@@ -36,3 +36,39 @@ function padStart(string, length, addedString) {
   const endLength = addCount % addedString.length;
   return addedString.slice(0, endLength) + addedString.repeat(repeatCount) + string;
 }
+
+// Cтрока короче 20 символов
+compareLength('проверяемая строка', 20); // true
+// Длина строки ровно 18 символов
+compareLength('проверяемая строка', 18); // true
+// Строка длиннее 10 символов
+compareLength('проверяемая строка', 10); // false
+
+// Строка является палиндромом
+isPalindrome('топот'); // true
+// Несмотря на разный регистр, тоже палиндром
+isPalindrome('ДовОд'); // true
+// Это не палиндром
+isPalindrome('Кекс'); // false
+// Это палиндром
+isPalindrome('Лёша на полке клопа нашёл '); // true
+
+extractNumbers('2023 год'); // 2023
+extractNumbers('ECMAScript 2022'); // 2022
+extractNumbers('1 кефир, 0.5 батона'); // 105
+extractNumbers('агент 007'); // 7
+extractNumbers('а я томат'); // NaN
+extractNumbers(2023); // 2023
+extractNumbers(-1); // 1
+extractNumbers(1.5); // 15
+
+// Добавочный символ использован один раз
+padStart('1', 2, '0'); // '01'
+// Добавочный символ использован три раза
+padStart('1', 4, '0'); // '0001'
+// Добавочные символы обрезаны с конца
+padStart('q', 4, 'werty'); // 'werq'
+// Добавочные символы использованы полтора раза
+padStart('q', 4, 'we'); // 'wweq'
+// Добавочные символы не использованы, исходная строка не изменена
+padStart('qwerty', 4, '0'); // 'qwerty'
