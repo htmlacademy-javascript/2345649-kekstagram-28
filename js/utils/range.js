440cb3fb0c78995b22a877a85d1f262e3a0f6fdc
EYE <EYE@EYEz>
2023-03-13 22:04:15 +0300

    Заменит класс на функцию-конструктор

--- js/utils/range.js
+++ js/utils/range.js
@@ -1,22 +1,14 @@
-export class Range {
-  #value;
-
-  constructor(min = 0, max = 100, step = 1, value) {
-    this.min = min;
-    this.max = max;
-    this.step = step;
-    this.#value = value || max;
-  }
-
-  increase() {
-    this.#value = Math.min(this.#value + this.step, this.max);
-  }
-
-  decrease() {
-    this.#value = Math.max(this.#value - this.step, this.min);
-  }
-
-  get value() {
-    return this.#value;
-  }
+export function Range(min = 0, max = 100, step = 1, value) {
+  this._min = min;
+  this._max = max;
+  this._step = step;
+  this.value = value || max;
 }
+
+Range.prototype.increase = function() {
+  this.value = Math.min(this.value + this._step, this._max);
+};
+
+Range.prototype.decrease = function() {
+  this.value = Math.max(this.value - this._step, this._min);
+};
